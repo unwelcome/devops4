58b560d95f0788e483475569aea982c1cfc91fd6
unwelcome <moyapohtaxd@mail.ru>
2025-12-06 18:40:36 +0300

    add http get request handler in gateway; update nginx conf

--- gateway/go.mod
+++ gateway/go.mod
@@ -9,6 +9,16 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/gofiber/fiber/v2 v2.52.10 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
--- gateway/main.go
+++ gateway/main.go
@@ -4,22 +4,51 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	pb "github.com/unwelcome/devops4/proto/gen"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
+type DiceResponse struct {
+	Value int32  `json:"value"`
+	Emoji string `json:"emoji"`
+}
+
 var diceEmojis = map[int32]string{
 	1: "⚀", 2: "⚁", 3: "⚂", 4: "⚃", 5: "⚄", 6: "⚅",
 }
 
+var diceClient pb.DiceServiceClient
+
 func main() {
+	// 1. Установка gRPC-соединения
+	conn := setupGrpcConnection()
+	defer conn.Close()
+
+	// Инициализация клиента
+	diceClient = pb.NewDiceServiceClient(conn)
+
+	// Настройка HTTP-сервера
+	app := fiber.New()
+
+	// Регистрируем обработчик для пути /dice
+	app.Get("/dice", diceHandler)
+
+	port := ":8080"
+
+	// Запускаем HTTP-сервер
+	if err := app.Listen(port); err != nil {
+		log.Fatalf("Fiber server failed: %v", err)
+	}
+}
+
+func setupGrpcConnection() *grpc.ClientConn {
 	// Загружаем CA, чтобы доверять сертификату Nginx
 	caPem, err := os.ReadFile("/certs/ca.crt")
 	if err != nil {
@@ -38,17 +67,35 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
+	return conn
+}
 
-	client := pb.NewDiceServiceClient(conn)
+func diceHandler(c *fiber.Ctx) error {
+	// Устанавливаем таймаут для gRPC-запроса
+	ctx, cancel := context.WithTimeout(c.Context(), 2*time.Second)
+	defer cancel()
 
-	for {
-		resp, err := client.Roll(context.Background(), &pb.RollRequest{OperationId: uuid.New().String()})
-		if err != nil {
-			log.Printf("Error rolling dice: %v", err)
-		} else {
-			fmt.Printf("Dice: %s (Value: %d)\n", diceEmojis[resp.Number], resp.Number)
-		}
-		time.Sleep(3 * time.Second)
+	operationID := uuid.New().String()
+
+	// Выполняем gRPC-запрос
+	resp, err := diceClient.Roll(ctx, &pb.RollRequest{OperationId: operationID})
+	if err != nil {
+		log.Printf("Error rolling dice (gRPC call failed): %v", err)
+
+		// Fiber позволяет легко отправлять статус и сообщение об ошибке
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   "Ошибка gRPC-запроса",
+			"message": err.Error(),
+		})
 	}
+
+	// Формируем ответ
+	httpResponse := DiceResponse{
+		Value: resp.Number,
+		Emoji: diceEmojis[resp.Number],
+	}
+
+	// Fiber автоматически устанавливает Content-Type: application/json
+	log.Printf("ID: %s; Method: GET; Path: /dice; Status: Success\n", operationID)
+	return c.JSON(httpResponse)
 }
